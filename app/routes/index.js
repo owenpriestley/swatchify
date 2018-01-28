fdce90bd23159a8135c7ac417dd2a3741ac4c680
Owen Priestley <owen.priestley@citizensadvice.org.uk>
2018-01-28 22:23:26 +0000

    clean up server

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,4 @@
 const contentRoutes = require('./content_routes');
 module.exports = function(app, db) {
   contentRoutes(app, db);
-
-// Other route groups could go here in the future
-
 };
--- server.js
+++ server.js
@@ -1,20 +1,14 @@
 // server.js //
-
-// gimme deets //
 require('dotenv').config();
 
 // declare dependencies //
 
 const express = require('express');
-//const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const db = require('./config/db');
 const app = express();
-
-// specify a port (8000) and start listening //
 const port = 8000;
 
-//tell server to use body-parser //
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('site'));
 app.use('/js/', express.static(__dirname + '/node_modules/'));
@@ -23,14 +17,3 @@ app.listen(port, () => {
     require('./app/routes')(app,);
     console.log('Stayin alive on port ' + port);
 });
-
-
-// connect to MongoDB //
-
-/*MongoClient.connect(db.url, (err, database) => {
-    if (err) return console.log(err)
-    require('./app/routes')(app, database);
-    app.listen(port, () => {
-        console.log('Stayin alive on port ' + port);
-    });
-}) */
