357872dddb496283724cc9e8578162d0d5e1ab90
Owen Priestley <owen.priestley@citizensadvice.org.uk>
2018-01-29 12:09:30 +0000

    fix layout

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -52,7 +52,7 @@ $(document).ready(function() {
         console.error(err);
       }
     );
-    $(".get").text("Palette");
+    $(".get").text("Swatches");
     $("h1").addClass("underline");
   });
 });
