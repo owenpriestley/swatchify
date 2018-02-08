e14a289ef7a0a8f892b9c19f42ca6bba3aa8ce62
Owen Priestley <owen.priestley@citizensadvice.org.uk>
2018-02-08 09:30:31 +0000

    clean up JS some more

--- site/scripts/main.js
+++ site/scripts/main.js
@@ -50,33 +50,3 @@ $(document).ready(function() {
     $("h1").addClass("underline");
   });
 });
-
-/* MODAL HELP, IF YA WANNIT
-
-    // Get the modal
-window.onload = function () {
-    var modal = document.getElementById("helpBox");
-
-    // Get the button that opens the modal
-    var btn = document.getElementById("helpButt");
-
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-
-    // When the user clicks on the button, open the modal 
-    btn.onclick = function () {
-        modal.style.display = "block";
-    }
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function () {
-        modal.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function (event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    } 
-}*/
